bec7f1395db930bc5856b7bccd1e607020d13c62
Ojeshwi Gurung <101081568+ojeshwi@users.noreply.github.com>
2023-01-19 21:49:26 +0545

    Update Main.java

--- family/src/Main.java
+++ family/src/Main.java
@@ -1,13 +1,24 @@
-import model.Parents;
 import model.Children;
 import model.Hobby;
-
 import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<String > childrenlist =new ArrayList<~>();
-        Children c1 = new Children("ojeshwi",12, 18, party);
+        ArrayList<Hobby> hobby1 =new ArrayList<>();
+        Hobby h1=new Hobby("chess","internal");
+        Hobby h2=new Hobby("badminton","external");
+        hobby1.add(h1);
+        hobby1.add(h2);
+        ArrayList<Hobby> hobby2 =new ArrayList<>();
+        Hobby h3=new Hobby("Swimming","external");
+        Hobby h4=new Hobby("Football","external");
+        hobby2.add(h3);
+        hobby2.add(h4);
+
+        Children child1=new Children("gurung","ojeshwi",12,18, hobby1);
+        Children child2=new Children("shrestha","aavarna",12,18,hobby2);
+
+        child1.format();
 
 
     }
