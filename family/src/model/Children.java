02e01093bfe530128fe5623eb410229094d37e70
Ojeshwi Gurung <101081568+ojeshwi@users.noreply.github.com>
2023-01-19 21:48:58 +0545

    Update Children.java

--- family/src/model/Children.java
+++ family/src/model/Children.java
@@ -1,25 +1,13 @@
 package model;
 
-import java.util.ArrayList;
+public class Hobby{
+    String name;
+    String type;
 
-public class Children extends Parents{
-    public String name;
-    int grade;
-    int age;
-    ArrayList<Hobby> Hobby;
+    public Hobby(String name, String type) {
 
-    public Children(String Parents_name, String name, int grade, int age, ArrayList<model.Hobby> hobby) {
-        super(Parents_name);
         this.name = name;
-        this.grade = grade;
-        this.age = age;
-        Hobby = hobby;
+        this.type = type;
     }
-    public void format(){
-        String name="Your name is"+" "+ this.name+" "+this.Parents_name;
-        System.out.println(name);
-        for (Hobby i : Hobby){
-            System.out.print("Your hobby is "+i);
-        }
-    }
+
 }
