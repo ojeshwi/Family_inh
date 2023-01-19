743b501aa33cccf4032901025325e3af93f5569c
Ojeshwi Gurung <101081568+ojeshwi@users.noreply.github.com>
2023-01-19 21:42:24 +0545

    Update Hobby.java

--- family/src/model/Hobby.java
+++ family/src/model/Hobby.java
@@ -1,24 +1,13 @@
 package model;
 
-import java.util.ArrayList;
+public class Hobby{
+    String name;
+    String type;
 
-public class Hobby {
-    public String name;
-    ArrayList type;
+    public Hobby(String name, String type) {
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
         this.name = name;
-    }
-
-    public ArrayList getType() {
-        return type;
-    }
-
-    public void setType(ArrayList type) {
         this.type = type;
     }
+
 }
