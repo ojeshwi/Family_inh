0a0d0b343d7e365cc679800d3340e594eb995d5f
Ojeshwi Gurung <101081568+ojeshwi@users.noreply.github.com>
2023-01-19 21:48:08 +0545

    Update Parents.java

--- family/src/model/Parents.java
+++ family/src/model/Parents.java
@@ -1,29 +1,10 @@
 package model;
 
-import java.util.ArrayList;
-
 public class Parents {
-    public String name;
-    ArrayList<Parents> house_Address;
-
-    public Parents(String name, ArrayList<Parents> house_Adddress){
-        this.name = name;
-        this.house_Address = house_Adddress;
+    public Parents(String Parents_name) {
+        this.Parents_name = Parents_name;
     }
 
-    public String getName() {
-        return name;
-    }
+    public String Parents_name;
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public ArrayList<Parents> getHouse_Address() {
-        return house_Address;
-    }
-
-    public void setHouse_Address(ArrayList<Parents> house_Address) {
-        this.house_Address = house_Address;
-    }
 }
